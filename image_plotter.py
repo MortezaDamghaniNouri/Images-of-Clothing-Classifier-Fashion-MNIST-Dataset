1130e0ccd5b50431c006fbd90b773a8d988fd056
Morteza Damghani <mortezadamghani77@gmail.com>
2022-12-19 03:09:48 -0500

    image_plotter.py finished.

--- image_plotter.py
+++ image_plotter.py
@@ -1,11 +1,16 @@
+"""image_plotter
+
+This script gets the index of an image in the Fashion MNIST dataset, and plots the image.
+"""
+
 import tensorflow as tf
-import numpy as np
 import matplotlib.pyplot as plt
 
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+plt.imshow(train_images[0])
+plt.show()
 
 
 
